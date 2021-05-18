197a0b6878885c5f60400e213dd36205441298bc
ItaloCanturil <italo.cantur@gmail.com>
2021-05-17 23:23:01 -0300

    Metodo construtor

--- POO/src/banco.php
+++ POO/src/banco.php
@@ -2,6 +2,6 @@
 
 require_once 'src/Conta.php';
 
-$primeiraConta = new Conta();
+$primeiraConta = new Conta('101.184.152-52', 'Italo Canturil');
 $primeiraConta->deposita(500);
 $primeiraConta->saca(200);
